e576deefeb628f4d8311678312bfd5778304756a
AndrehAguiar <andreaguiar.g@gmail.com>
2021-09-09 15:38:56 -0300

    create feed form

--- frontend/src/app/feed/containers/feed/feed.component.ts
+++ frontend/src/app/feed/containers/feed/feed.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'fd-feed',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feed.component.css']
 })
 export class FeedComponent implements OnInit {
+  
+  faGithub = faGithub;
 
   constructor() { }
 
--- frontend/src/app/feed/feed.module.ts
+++ frontend/src/app/feed/feed.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FeedComponent } from './containers/feed/feed.component';
 import { FeedRoutingModule } from './feed-routing.module';
 
@@ -12,6 +12,7 @@ import { FeedRoutingModule } from './feed-routing.module';
   imports: [
     CommonModule,
     FeedRoutingModule,
+    FontAwesomeModule, 
   ]
 })
 export class FeedModule { }
