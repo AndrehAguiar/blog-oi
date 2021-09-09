1b60a31d8b9b5545eb1ac565054edc06b24afea3
AndrehAguiar <andreaguiar.g@gmail.com>
2021-09-08 23:41:32 -0300

    Create home

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
 import { FooterComponent } from './shared/footer/footer.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 
@@ -16,7 +17,8 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,    
+    HomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/shared/footer/footer.component.ts
+++ frontend/src/app/shared/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
@@ -11,6 +11,8 @@ export class FooterComponent implements OnInit {
 
   faLinkedin = faLinkedin;
   faGithub = faGithub;
+
+  @Input() title!: string;
 
   constructor() { }
 
